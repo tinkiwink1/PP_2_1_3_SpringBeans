d0ba89879b21be19c8ea5e6e0eec864fb1c39fcf
Ildar Gimadutdinov <tinkiwinki1443@gmail.com>
2024-10-21 22:27:13 +0400

    Add class Dog and setting Annotations

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,19 +1,33 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+
+import java.beans.BeanProperty;
 
 @Component
 public class AnimalsCage {
 
     @Autowired
+    @Qualifier("dog")
     private Animal animal;
+
+    @Autowired
+    @Qualifier("timer")
+    private Timer timer;
+
+    @Bean
+    public Timer getTimer() {
+        return this.timer;
+    }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());
         System.out.println("________________________");
     }
 }
